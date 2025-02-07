a283ede0b578b26115d38edd62408d2b66b06560
Leven <leven890619@gmail.com>
2025-02-07 14:54:26 +0800

    work version

--- src/sniperIBox.ts
+++ src/sniperIBox.ts
@@ -1,24 +1,53 @@
-import { Connection, Keypair, PublicKey } from '@solana/web3.js';
+import { Connection, Keypair, PublicKey, sendAndConfirmTransaction, Transaction, VersionedTransaction } from '@solana/web3.js';
 import {
-  ApiV3PoolInfoStandardItemCpmm,
-  CpmmKeys,
-  CpmmRpcData,
-  CREATE_CPMM_POOL_PROGRAM, CurveCalculator, Raydium
+  API_URLS,
+  CREATE_CPMM_POOL_PROGRAM,
+  parseTokenAccountResp,
+  Raydium
 } from '@raydium-io/raydium-sdk-v2'
-import BN from 'bn.js'
+import axios from 'axios';
 import bs58 from 'bs58';
 import dotenv from "dotenv";
-import { NATIVE_MINT } from '@solana/spl-token';
-import { isValidCpmm } from './utils/config';
+import { NATIVE_MINT, TOKEN_2022_PROGRAM_ID, TOKEN_PROGRAM_ID } from '@solana/spl-token';
 dotenv.config();
 
+interface SwapCompute {
+  id: string
+  success: true
+  version: 'V0' | 'V1'
+  openTime?: undefined
+  msg: undefined
+  data: {
+    swapType: 'BaseIn' | 'BaseOut'
+    inputMint: string
+    inputAmount: string
+    outputMint: string
+    outputAmount: string
+    otherAmountThreshold: string
+    slippageBps: number
+    priceImpactPct: number
+    routePlan: {
+      poolId: string
+      inputMint: string
+      outputMint: string
+      feeMint: string
+      feeRate: number
+      feeAmount: string
+    }[]
+  }
+}
 class RaydiumLiquidityMonitor {
   private connection: Connection;
   private keypair: Keypair | null = null;
   private raydium: Raydium | null = null;
+  private isBuying: boolean = false;
 
   constructor() {
-    this.keypair = Keypair.fromSecretKey(bs58.decode(process.env.PRIVATE_KEY!));
+    const privateKeyString = process.env.PRIVATE_KEY;
+    if (!privateKeyString) {
+      throw new Error('PRIVATE_KEY environment variable is not set');
+    }
+    this.keypair = Keypair.fromSecretKey(bs58.decode(privateKeyString));
     this.connection = new Connection(process.env.HTTPS_ENDPOINT!, {
       wsEndpoint: process.env.WSS_ENDPOINT!,
       commitment: 'confirmed',
@@ -39,7 +68,7 @@ class RaydiumLiquidityMonitor {
         connection: this.connection,
         cluster: 'mainnet',
         disableFeatureCheck: true,
-        disableLoadToken: false,
+        disableLoadToken: true,
         blockhashCommitment: 'confirmed',
       });
     }
@@ -76,21 +105,21 @@ class RaydiumLiquidityMonitor {
                     const poolId = new PublicKey(tx?.transaction.message.staticAccountKeys[2].toBase58()!);
                     console.log("Found pool:", poolId.toString(), new Date().toISOString());
                     const tokenA = new PublicKey(tx?.transaction.message.staticAccountKeys[len - 1].toBase58()!);
-                    console.log("Found token:", tokenA.toString(), new Date().toISOString());
+                    console.log("Found base token:", tokenA.toString(), new Date().toISOString());
                     const tokenB = new PublicKey(tx?.transaction.message.staticAccountKeys[len - 5].toBase58()!);
-                    console.log("Found token:", tokenB.toString(), new Date().toISOString());
-                    if(!this.isIBoxToken(tokenA, tokenB)) {
+                    console.log("Found quote token:", tokenB.toString(), new Date().toISOString());
+                    if(!this.isIBoxToken(tokenA, tokenB) || this.isBuying) {
                         return
                     }
 
                     // 买入代币
-                    // try {
-                    //   console.log('开始买入代币...');
-                    //   const signature = await this.buyToken(poolId, 0.5, 50);
-                    //   console.log('买入成功，交易签名:', signature);
-                    // } catch (error) {
-                    //     console.error('买入失败:', error);
-                    // }
+                    try {
+                      console.log('开始买入代币...');
+                      const txId = await this.swap(tokenA);
+                      console.log('买入成功，交易签名:', txId);
+                    } catch (error) {
+                        console.error('买入失败:', error);
+                    }
                 } catch (error) {
                     console.error('获取Base Token信息失败:', error)
                 }
@@ -108,65 +137,192 @@ class RaydiumLiquidityMonitor {
     }
   }
 
-  private async buyToken(poolId: PublicKey, amountIn: number, slippage: number) {
+  private async swap(tokenA: PublicKey) : Promise<string | null> {
     if(!this.raydium) throw new Error('Raydium not initialized')
     if(!this.connection) throw new Error('Connection not initialized')
     if(!this.keypair) throw new Error('Keypair not initialized')
+    this.isBuying = true;
 
-    const inputAmount = new BN(100)
     const inputMint = NATIVE_MINT.toBase58()
-    let poolInfo: ApiV3PoolInfoStandardItemCpmm
-    let poolKeys: CpmmKeys | undefined
-    let rpcData: CpmmRpcData | undefined
-    const data = await this.raydium.api.fetchPoolById({ ids: poolId.toBase58() })
-    poolInfo = data[0] as ApiV3PoolInfoStandardItemCpmm
-    if (!isValidCpmm(poolInfo.programId)) throw new Error('target pool is not CPMM pool')
-    rpcData = await this.raydium.cpmm.getRpcPoolInfo(poolInfo.id, true)
+    const outputMint = tokenA.toBase58()
+    const amount = 10000
+    const slippage = 0.5 // in percent, for this example, 0.5 means 0.5%
+    const txVersion: string = 'V0' // or LEGACY
+    const isV0Tx = txVersion === 'V0'
+    console.log('start swap....')
 
-    if (inputMint !== poolInfo.mintA.address && inputMint !== poolInfo.mintB.address)
-      throw new Error('input mint does not match pool')
-    
-    const baseIn = inputMint === poolInfo.mintA.address
-    // swap pool mintA for mintB
-    const swapResult = CurveCalculator.swap(
-      inputAmount,
-      baseIn ? rpcData.baseReserve : rpcData.quoteReserve,
-      baseIn ? rpcData.quoteReserve : rpcData.baseReserve,
-      rpcData.configInfo!.tradeFeeRate
+    const [isInputSol, isOutputSol] = [inputMint === NATIVE_MINT.toBase58(), outputMint === NATIVE_MINT.toBase58()]
+    console.log('isInputSol:', isInputSol, 'isOutputSol:', isOutputSol)
+    const { tokenAccounts } = await this.fetchTokenAccountData()
+    const inputTokenAcc = tokenAccounts.find((a) => a.mint.toBase58() === inputMint)?.publicKey
+    const outputTokenAcc = tokenAccounts.find((a) => a.mint.toBase58() === outputMint)?.publicKey
+    console.log('inputTokenAcc:', inputTokenAcc?.toBase58(), 'outputTokenAcc:', outputTokenAcc?.toBase58())
+    if (!inputTokenAcc && !isInputSol) {
+      console.error('do not have input token account')
+      return null
+    }
+    console.log(`url: ${API_URLS.BASE_HOST}${API_URLS.PRIORITY_FEE}`)
+    const { data } = await axios.get<{
+      id: string
+      success: boolean
+      data: { default: { vh: number; h: number; m: number } }
+    }>(`${API_URLS.BASE_HOST}${API_URLS.PRIORITY_FEE}`)
+    console.log('data:', data)
+
+    const { data: swapResponse } = await axios.get<SwapCompute>(
+      `${
+        API_URLS.SWAP_HOST
+      }/compute/swap-base-in?inputMint=${inputMint}&outputMint=${outputMint}&amount=${amount}&slippageBps=${
+        slippage * 100
+      }&txVersion=${txVersion}`
     )
+    console.log('swapResponse:', swapResponse)
+    if (!swapResponse.success && swapResponse.openTime) {
+      const openTime = parseInt(swapResponse.openTime) * 1000; // 转换为毫秒
+      const now = Date.now();
+      if (openTime > now) {
+        const delayMs = openTime - now;
+        console.log(`池子将在 ${new Date(openTime).toLocaleString()} 开放，等待 ${Math.floor(delayMs/1000)} 秒...`);
+        await new Promise(resolve => setTimeout(resolve, delayMs));
+        // 重新尝试交易
+        return await this.swap(tokenA);
+      }
+    }
 
-    /**
-     * swapResult.sourceAmountSwapped -> input amount
-     * swapResult.destinationAmountSwapped -> output amount
-     * swapResult.tradeFee -> this swap fee, charge input mint
-     */
-    const { execute } = await this.raydium.cpmm.swap({
-      poolInfo,
-      poolKeys,
-      inputAmount,
-      swapResult,
-      slippage: 0.1, // range: 1 ~ 0.0001, means 100% ~ 0.01%
-      baseIn,
-      // optional: set up priority fee here
-      // computeBudgetConfig: {
-      //   units: 600000,
-      //   microLamports: 4659150,
-      // },
-
-      // optional: add transfer sol to tip account instruction. e.g sent tip to jito
-      // txTipConfig: {
-      //   address: new PublicKey('96gYZGLnJYVFmbjzopPSU6QiEV5fGqZNyN9nmNhvrZU5'),
-      //   amount: new BN(10000000), // 0.01 sol
-      // },
+    const { data: swapTransactions } = await axios.post<{
+      id: string
+      version: string
+      success: boolean
+      data: { transaction: string }[]
+    }>(`${API_URLS.SWAP_HOST}/transaction/swap-base-in`, {
+      computeUnitPriceMicroLamports: String(data.data.default.h),
+      swapResponse,
+      txVersion,
+      wallet: this.keypair!.publicKey.toBase58(),
+      wrapSol: isInputSol,
+      unwrapSol: isOutputSol, // true means output mint receive sol, false means output mint received wsol
+      inputAccount: isInputSol ? undefined : inputTokenAcc?.toBase58(),
+      outputAccount: isOutputSol ? undefined : outputTokenAcc?.toBase58(),
     })
-
-    // don't want to wait confirm, set sendAndConfirm to false or don't pass any params to execute
-    const { txId } = await execute({ sendAndConfirm: true })
-    console.log(`swapped: ${poolInfo.mintA.symbol} to ${poolInfo.mintB.symbol}:`, {
-      txId: `https://explorer.solana.com/tx/${txId}`,
-    })
-    // process.exit() // if you don't want to end up node execution, comment this line
+    console.log('swapTransactions:', swapTransactions)
+  
+    const allTxBuf = swapTransactions.data.map((tx) => Buffer.from(tx.transaction, 'base64'))
+    const allTransactions = allTxBuf.map((txBuf) =>
+      isV0Tx ? VersionedTransaction.deserialize(txBuf) : Transaction.from(txBuf)
+    )
+  
+    console.log(`total ${allTransactions.length} transactions`, swapTransactions)
+  
+    let idx = 0
+    let txId = ''
+    if (!isV0Tx) {
+      for (const tx of allTransactions) {
+        console.log(`${++idx} transaction sending...`)
+        const transaction = tx as Transaction
+        transaction.sign(this.keypair!)
+        txId = await sendAndConfirmTransaction(this.connection, transaction, [this.keypair!], { skipPreflight: true })
+        console.log(`${++idx} transaction confirmed, txId: ${txId}`)
+      }
+    } else {
+      for (const tx of allTransactions) {
+        idx++
+        const transaction = tx as VersionedTransaction
+        transaction.sign([this.keypair!])
+        txId = await this.connection.sendTransaction(tx as VersionedTransaction, { skipPreflight: true })
+        const { lastValidBlockHeight, blockhash } = await this.connection.getLatestBlockhash({
+          commitment: 'confirmed',
+        })
+        console.log(`${idx} transaction sending..., txId: ${txId}`)
+        await this.connection.confirmTransaction(
+          {
+            blockhash,
+            lastValidBlockHeight,
+            signature: txId,
+          },
+          'confirmed'
+        )
+        console.log(`${idx} transaction confirmed`)
+      }
+    }
+    return txId
+    //process.exit()
   }
+
+  private fetchTokenAccountData = async () => {
+    const solAccountResp = await this.connection.getAccountInfo(this.keypair!.publicKey)
+    const tokenAccountResp = await this.connection.getTokenAccountsByOwner(this.keypair!.publicKey, { programId: TOKEN_PROGRAM_ID })
+    const token2022Req = await this.connection.getTokenAccountsByOwner(this.keypair!.publicKey, { programId: TOKEN_2022_PROGRAM_ID })
+    const tokenAccountData = parseTokenAccountResp({
+      owner: this.keypair!.publicKey,
+      solAccountResp,
+      tokenAccountResp: {
+        context: tokenAccountResp.context,
+        value: [...tokenAccountResp.value, ...token2022Req.value],
+      },
+    })
+    return tokenAccountData
+  }
+
+  // private async buyToken(poolId: PublicKey, amountIn: number, slippage: number) {
+  //   if(!this.raydium) throw new Error('Raydium not initialized')
+  //   if(!this.connection) throw new Error('Connection not initialized')
+  //   if(!this.keypair) throw new Error('Keypair not initialized')
+
+  //   const inputAmount = new BN(1000000) //1000000000
+  //   const inputMint = NATIVE_MINT.toBase58()
+  //   let poolInfo: ApiV3PoolInfoStandardItemCpmm
+  //   let poolKeys: CpmmKeys | undefined
+  //   let rpcData: CpmmRpcData | undefined
+  //   const data = await this.raydium.api.fetchPoolById({ ids: poolId.toBase58() })
+  //   if(!data) throw new Error('Empty. Failed to fetch pool')
+  //   poolInfo = data[0] as ApiV3PoolInfoStandardItemCpmm
+  //   if (!isValidCpmm(poolInfo.programId)) throw new Error('target pool is not CPMM pool')
+  //   rpcData = await this.raydium.cpmm.getRpcPoolInfo(poolInfo.id, true)
+
+  //   if (inputMint !== poolInfo.mintA.address && inputMint !== poolInfo.mintB.address)
+  //     throw new Error('input mint does not match pool')
+    
+  //   const baseIn = inputMint === poolInfo.mintA.address
+  //   // swap pool mintA for mintB
+  //   const swapResult = CurveCalculator.swap(
+  //     inputAmount,
+  //     baseIn ? rpcData.baseReserve : rpcData.quoteReserve,
+  //     baseIn ? rpcData.quoteReserve : rpcData.baseReserve,
+  //     rpcData.configInfo!.tradeFeeRate
+  //   )
+
+  //   /**
+  //    * swapResult.sourceAmountSwapped -> input amount
+  //    * swapResult.destinationAmountSwapped -> output amount
+  //    * swapResult.tradeFee -> this swap fee, charge input mint
+  //    */
+  //   const { execute } = await this.raydium.cpmm.swap({
+  //     poolInfo,
+  //     poolKeys,
+  //     inputAmount,
+  //     swapResult,
+  //     slippage: 0.1, // range: 1 ~ 0.0001, means 100% ~ 0.01%
+  //     baseIn,
+  //     // optional: set up priority fee here
+  //     // computeBudgetConfig: {
+  //     //   units: 600000,
+  //     //   microLamports: 4659150,
+  //     // },
+
+  //     // optional: add transfer sol to tip account instruction. e.g sent tip to jito
+  //     // txTipConfig: {
+  //     //   address: new PublicKey('96gYZGLnJYVFmbjzopPSU6QiEV5fGqZNyN9nmNhvrZU5'),
+  //     //   amount: new BN(10000000), // 0.01 sol
+  //     // },
+  //   })
+
+  //   // don't want to wait confirm, set sendAndConfirm to false or don't pass any params to execute
+  //   const { txId } = await execute({ sendAndConfirm: true })
+  //   console.log(`swapped: ${poolInfo.mintA.symbol} to ${poolInfo.mintB.symbol}:`, {
+  //     txId: `https://explorer.solana.com/tx/${txId}`,
+  //   })
+  //   process.exit() // if you don't want to end up node execution, comment this line
+  // }
 
   private isIBoxToken(tokenAMint: PublicKey, tokenBMint: PublicKey): boolean {
     // const mintAddress = tokenAMint.toBase58();
